8d6e1198bbfb0b6d9c3813ec68b7bdee7e6dfb0a
Andras Sore <andras08@gmail.com>
2021-08-26 23:41:42 +0200

    fix spaces

--- src/DueDateCalculator.ts
+++ src/DueDateCalculator.ts
@@ -13,62 +13,62 @@ const FRIDAY_INDEX = 5;
 const FRIDAY_TO_MONDAY_DAYS = 3; 
 
 export default class DueDateCalculator {
-	private readonly workdayStartHour: number;
-	private readonly workdayEndHour: number;
+    private readonly workdayStartHour: number;
+    private readonly workdayEndHour: number;
 
-	constructor(options: Options = DefaultOptions) {
-		this.workdayStartHour = options.workdayStartHour;
-		this.workdayEndHour = options.workdayEndHour;
-	}
+    constructor(options: Options = DefaultOptions) {
+        this.workdayStartHour = options.workdayStartHour;
+        this.workdayEndHour = options.workdayEndHour;
+    }
 
-	calculateDueDate(submitTime: Date, turnaroundHours: number) {
+    calculateDueDate(submitTime: Date, turnaroundHours: number) {
         this.validateSubmitTime(submitTime);
 
-		const { fullWorkdays, remainderHours } = this.convertToWorkdays(turnaroundHours);
+        const { fullWorkdays, remainderHours } = this.convertToWorkdays(turnaroundHours);
 
-		let dueDate = new Date(submitTime);
+        let dueDate = new Date(submitTime);
 
-		for(let i = 0; i < fullWorkdays; i++) {
-			this.setToNextWorkingDay(dueDate);
-		}
+        for(let i = 0; i < fullWorkdays; i++) {
+            this.setToNextWorkingDay(dueDate);
+        }
 
-		for(let i = 0; i < remainderHours; i++) {
-			this.setToNextWorkingHour(dueDate);
-		}
+        for(let i = 0; i < remainderHours; i++) {
+            this.setToNextWorkingHour(dueDate);
+        }
 
         return dueDate;
-	}
+    }
 
-	private get workdayHours() {
-		return (this.workdayEndHour - this.workdayStartHour);
-	}
+    private get workdayHours() {
+        return (this.workdayEndHour - this.workdayStartHour);
+    }
 
-	private setToNextWorkingDay(d: Date) {
-		const isFriday = d.getDay() === FRIDAY_INDEX;
+    private setToNextWorkingDay(d: Date) {
+        const isFriday = d.getDay() === FRIDAY_INDEX;
 
-		if (isFriday) {
-			d.setDate(d.getDate() + FRIDAY_TO_MONDAY_DAYS);
+        if (isFriday) {
+            d.setDate(d.getDate() + FRIDAY_TO_MONDAY_DAYS);
         } else {
-			d.setDate(d.getDate() + 1);
-		}
-	}
+            d.setDate(d.getDate() + 1);
+        }
+    }
 
-	private setToNextWorkingHour(d: Date) {
-		const isLastHour = d.getHours() === this.workdayEndHour - 1;
+    private setToNextWorkingHour(d: Date) {
+        const isLastHour = d.getHours() === this.workdayEndHour - 1;
 
-		if (isLastHour) {
-			this.setToNextWorkingDay(d);
-			d.setHours(this.workdayStartHour);
-		} else {
-			d.setHours(d.getHours() + 1);
-		}
-	}
+        if (isLastHour) {
+            this.setToNextWorkingDay(d);
+            d.setHours(this.workdayStartHour);
+        } else {
+            d.setHours(d.getHours() + 1);
+        }
+    }
 
-	private convertToWorkdays(turnaroundHours: number) {
-		const fullWorkdays = Math.floor(turnaroundHours / this.workdayHours);
-		const remainderHours = turnaroundHours % this.workdayHours;
-		return { fullWorkdays, remainderHours };
-	}
+    private convertToWorkdays(turnaroundHours: number) {
+        const fullWorkdays = Math.floor(turnaroundHours / this.workdayHours);
+        const remainderHours = turnaroundHours % this.workdayHours;
+        return { fullWorkdays, remainderHours };
+    }
 
     private validateSubmitTime(submitTime: Date): void {
         const submitHours = submitTime.getHours();
